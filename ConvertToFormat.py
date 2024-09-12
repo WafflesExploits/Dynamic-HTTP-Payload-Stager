29266cbca18989a52cab87cff377499612adc4aa
André Oliveira <andreoliveira208@gmail.com>
2024-09-12 16:35:19 +0100

    Update ConvertToFormat.py

--- ConvertToFormat.py
+++ ConvertToFormat.py
@@ -2,19 +2,36 @@
 # ./ConvertToFormat.py
 # Made by WafflesExploits
 from base64 import b64encode 
+from sys import argv
 
-#--Replace these values--
-delimiter = "-"
-variables = """
-unsigned char Encrypted_Shellcode[] = {0x99,0x12 }; 
+# These variables are not to be changed.
+delimiter = ""
+output_path = ""
+arguments_count = len(argv)
 
+if(arguments_count < 2):
+    print("./ConvertToFormat.py <file> [<delimiter> <output-path>]")
+    print("[i] File content example: ")
+    print("""unsigned char Encrypted_Shellcode[] = {0x99,0x12 }; 
 unsigned char Encrypted_Key[] = {0xF6,0x6B}; 
-
 unsigned char Protected_Key_for_bruteforce[] = {0x00,0xF0}; 
+BYTE Hint_Byte = 0xCB;""")
+    exit()
+elif(arguments_count < 3):
+    delimiter = "-"
+elif(arguments_count < 4):
+    delimiter = argv[2]
+else:
+    delimiter = argv[2]
+    output_path = argv[3]
+    
 
-BYTE Hint_Byte = 0xCB;"""
+file_path = argv[1]
 
-#--Script Start--
+f = open(file_path, "r")
+variables = f.read()
+
+
 # Remove strings
 text = variables
 text = text.replace("\n","");
@@ -38,7 +55,15 @@ text_bytes = text.encode('utf-8')
 
 # Now, base64 encode the bytes
 encoded_text = b64encode(text_bytes).decode('utf-8')
-print(encoded_text);
-
-# Output:
-#RW5jcnlwdGVkX1NoZWxsY29kZS0weDk5LDB4MTIgIApFbmNyeXB0ZWRfS2V5LTB4RjYsMHg2QiAKUHJvdGVjdGVkX0tleV9mb3JfYnJ1dGVmb3JjZS0weDAwLDB4RjAgCkhpbnRfQnl0ZS0weENCCg==
+if(output_path):
+    f = open(output_path, "w")
+    f.write(encoded_text)
+    f.close()
+    print(f"[i] Using delimiter \"{delimiter}\"")
+    print(f"[i] Saved file to \"{output_path}\"")
+else:
+    print(encoded_text)
+    if(arguments_count < 3):
+        print("[i] Using default delimiter \"-\"")
+    else:
+        print(f"[i] Using delimiter \"{delimiter}\"")
